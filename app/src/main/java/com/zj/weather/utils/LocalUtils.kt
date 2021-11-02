39def33435ad5a169aecb41ba248c5146ae5e50f
zhujiang2 <zhujiang2@lenovo.com>
2021-11-02 09:11:59 +0800

    多语言适配

--- app/src/main/java/com/zj/weather/utils/LocalUtils.kt
+++ app/src/main/java/com/zj/weather/utils/LocalUtils.kt
@@ -11,7 +11,7 @@ fun getDefaultLocale(context: Context?): Lang {
     if (context == null) return Lang.ZH_HANS
     return when (context.resources.configuration.locale.language) {
         "zh" -> Lang.ZH_HANS
-        "zh_HK", "zh_TW" -> Lang.ZH_HANT
+        "zh_rHK", "zh_rTW" -> Lang.ZH_HANT
         "es", "en" -> Lang.EN
         else -> Lang.EN
     }
