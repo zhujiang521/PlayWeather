8be4f54f2c10aab32e8ac3c74b5e8000f30e1eab
zhujiang2 <zhujiang2@lenovo.com>
2021-11-02 10:09:30 +0800

    升级版本号

--- app/src/main/java/com/zj/weather/utils/LocalUtils.kt
+++ app/src/main/java/com/zj/weather/utils/LocalUtils.kt
@@ -12,7 +12,7 @@ fun getDefaultLocale(context: Context?): Lang {
     if (context == null) return Lang.ZH_HANS
     Log.e("TAG", "getDefaultLocale: ${context.resources.configuration.locales[0].toLanguageTag()}")
     return when (context.resources.configuration.locales[0].toLanguageTag()) {
-        "zh" -> Lang.ZH_HANS
+        "zh", "zh-CN" -> Lang.ZH_HANS
         "zh_rHK", "zh_rTW", "zh_HK", "zh_TW", "HK", "TW", "zh-TW", "zh-HK" -> Lang.ZH_HANT
         "es", "en" -> Lang.EN
         else -> Lang.EN
