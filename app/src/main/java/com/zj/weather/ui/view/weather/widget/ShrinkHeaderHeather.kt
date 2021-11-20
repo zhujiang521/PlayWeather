fc2453e6e16cf8ddf0455724170527b5b0e50480
zhujiang2 <zhujiang2@lenovo.com>
2021-11-20 10:29:09 +0800

    优化折叠起来的天气信息

--- app/src/main/java/com/zj/weather/ui/view/weather/widget/ShrinkHeaderHeather.kt
+++ app/src/main/java/com/zj/weather/ui/view/weather/widget/ShrinkHeaderHeather.kt
@@ -14,12 +14,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.qweather.sdk.bean.weather.WeatherNowBean
+import com.zj.weather.R
 import com.zj.weather.room.entity.CityInfo
 
 @OptIn(ExperimentalAnimationApi::class)
@@ -77,7 +79,7 @@ fun ShrinkHeaderHeather(
                 }
 
                 Text(
-                    text = "${weatherNow?.text}  ${weatherNow?.temp}℃",
+                    text = "${weatherNow?.text ?: stringResource(id = R.string.default_weather)}  ${weatherNow?.temp ?: "0"}℃",
                     fontSize = 20.sp,
                     color = MaterialTheme.colors.primary
                 )
