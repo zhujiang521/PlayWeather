715498e7ba823c8ba260bcc84ef4544e03e0f895
zhujiang2 <zhujiang2@lenovo.com>
2021-12-09 22:50:04 +0800

    优化小部件，升级Gradle，适配资源文件

--- app/src/main/java/com/zj/weather/common/widget/today/TodayWeatherWidget.kt
+++ app/src/main/java/com/zj/weather/common/widget/today/TodayWeatherWidget.kt
@@ -1,5 +1,6 @@
 package com.zj.weather.common.widget.today
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
@@ -8,6 +9,7 @@ import android.net.Uri
 import android.os.Build
 import android.util.SizeF
 import android.widget.RemoteViews
+import com.zj.weather.MainActivity
 import com.zj.weather.R
 import com.zj.weather.common.widget.WeatherWidgetUtils
 import com.zj.weather.common.widget.utils.deleteCityInfoPref
@@ -17,6 +19,7 @@ import com.zj.weather.ui.view.weather.getLocation
 import com.zj.weather.utils.XLog
 import com.zj.weather.utils.weather.IconUtils
 
+const val CLICK_TODAY_ACTION = "com.zj.weather.common.widget.today.CLICK_TODAY_ACTION"
 
 /**
  * Implementation of App Widget functionality.
@@ -32,6 +35,9 @@ class TodayWeatherWidget : AppWidgetProvider() {
             AppWidgetManager.INVALID_APPWIDGET_ID
         )
         val cityInfo = loadCityInfoPref(context, appWidgetId, TODAY_PREFS_NAME)
+        if (intent.action == CLICK_TODAY_ACTION) {
+            MainActivity.actionNewStart(context, cityInfo)
+        }
         XLog.e("cityInfo:$cityInfo")
     }
 
@@ -70,7 +76,6 @@ internal fun updateAppWidget(
     appWidgetId: Int
 ) {
     val cityInfo = loadCityInfoPref(context, appWidgetId, TODAY_PREFS_NAME)
-    // Construct the RemoteViews object
     // 指示小部件管理器更新小部件
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
         val smallView = RemoteViews(context.packageName, R.layout.today_weather_widget_small)
@@ -89,16 +94,16 @@ internal fun updateAppWidget(
             SizeF(170f, 70f) to largeView,
             SizeF(170f, 110f) to largeView
         )
-        buildRemoteViews(context, cityInfo, smallView, viewMapping, appWidgetManager, appWidgetId)
-        buildRemoteViews(context, cityInfo, mediumView, viewMapping, appWidgetManager, appWidgetId)
-        buildRemoteViews(context, cityInfo, largeView, viewMapping, appWidgetManager, appWidgetId)
+        buildRemoteViews(context, cityInfo, smallView, appWidgetManager, appWidgetId, viewMapping)
+        buildRemoteViews(context, cityInfo, mediumView, appWidgetManager, appWidgetId, viewMapping)
+        buildRemoteViews(context, cityInfo, largeView, appWidgetManager, appWidgetId, viewMapping)
         buildRemoteViews(
             context,
             cityInfo,
             smallLargeView,
-            viewMapping,
             appWidgetManager,
-            appWidgetId
+            appWidgetId,
+            viewMapping
         )
     } else {
         val views = RemoteViews(context.packageName, R.layout.today_weather_widget_medium)
@@ -110,9 +115,9 @@ private fun buildRemoteViews(
     context: Context,
     cityInfo: CityInfo?,
     views: RemoteViews,
-    viewMapping: Map<SizeF, RemoteViews>,
     appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
+    appWidgetId: Int,
+    viewMapping: Map<SizeF, RemoteViews>? = null,
 ) {
     WeatherWidgetUtils.getWeather7Day(
         context = context,
@@ -132,20 +137,12 @@ private fun buildRemoteViews(
             IconUtils.getWeatherIcon(tomorrowBaseBean.icon)
         )
         views.setTextViewText(R.id.today_tv_tomorrow_text, tomorrowBaseBean.text)
-        views.setTextViewText(
-            R.id.today_tv_tomorrow_temp,
-            "${tomorrowBaseBean.min}/${tomorrowBaseBean.max}℃"
-        )
         val afterBaseBean = items[2]
         views.setImageViewResource(
             R.id.today_iv_after,
             IconUtils.getWeatherIcon(afterBaseBean.icon)
         )
         views.setTextViewText(R.id.today_tv_after_text, afterBaseBean.text)
-        views.setTextViewText(
-            R.id.today_tv_after_temp,
-            "${afterBaseBean.min}/${afterBaseBean.max}℃"
-        )
         // 构建适配器
         val intent = Intent(context, TodayWeatherWidgetService::class.java).apply {
             // Add the app widget ID to the intent extras.
@@ -155,77 +152,18 @@ private fun buildRemoteViews(
         }
         // 设置 RemoteViews 对象以使用 RemoteViews 适配器
         views.setRemoteAdapter(R.id.today_list_view, intent)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val itemClick = PendingIntent.getBroadcast(
+            context, 0,
+            Intent(context, TodayWeatherWidget::class.java)
+                .setAction(CLICK_TODAY_ACTION),
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        views.setOnClickPendingIntent(R.id.today_ll, itemClick)
+        views.setOnClickPendingIntent(R.id.today_rl, itemClick)
+        if (viewMapping != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             appWidgetManager.updateAppWidget(appWidgetId, RemoteViews(viewMapping))
+        } else {
+            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
-    }
-}
-
-private fun buildRemoteViews(
-    context: Context,
-    cityInfo: CityInfo?,
-    views: RemoteViews,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-//    WeatherWidgetUtils.getWeatherNow(context, cityInfo) { nowBaseBean ->
-//        XLog.e("nowBaseBean:${cityInfo?.name}  : ${nowBaseBean.temp}")
-//        views.setTextViewText(R.id.today_tv_location, "${cityInfo?.city} ${cityInfo?.name}")
-//        views.setTextViewText(R.id.today_tv_temp, "${nowBaseBean.temp}℃")
-//        views.setImageViewResource(
-//            R.id.widget_iv_icon,
-//            IconUtils.getWeatherIcon(nowBaseBean.icon)
-//        )
-//        views.setTextViewText(R.id.today_tv_text, nowBaseBean.text)
-//        appWidgetManager.updateAppWidget(appWidgetId, views)
-//    }
-
-    WeatherWidgetUtils.getWeather7Day(
-        context = context,
-        location = getLocation(cityInfo = cityInfo)
-    ) { items ->
-        val nowBaseBean = items[0]
-        views.setTextViewText(R.id.today_tv_location, "${cityInfo?.city} ${cityInfo?.name}")
-        views.setTextViewText(R.id.today_tv_temp, "${nowBaseBean.min}/${nowBaseBean.max}℃")
-        views.setImageViewResource(
-            R.id.widget_iv_icon,
-            IconUtils.getWeatherIcon(nowBaseBean.icon)
-        )
-        views.setTextViewText(R.id.today_tv_text, nowBaseBean.text)
-        val tomorrowBaseBean = items[1]
-        views.setImageViewResource(
-            R.id.today_iv_tomorrow,
-            IconUtils.getWeatherIcon(tomorrowBaseBean.icon)
-        )
-        views.setTextViewText(R.id.today_tv_tomorrow_text, tomorrowBaseBean.text)
-        views.setTextViewText(
-            R.id.today_tv_tomorrow_temp,
-            "${tomorrowBaseBean.min}/${tomorrowBaseBean.max}℃"
-        )
-        if (views.layoutId == R.layout.today_weather_widget_medium || views.layoutId == R.layout.today_weather_widget_large) {
-            val afterBaseBean = items[2]
-            views.setImageViewResource(
-                R.id.today_iv_after,
-                IconUtils.getWeatherIcon(afterBaseBean.icon)
-            )
-            views.setTextViewText(R.id.today_tv_after_text, afterBaseBean.text)
-            views.setTextViewText(
-                R.id.today_tv_after_temp,
-                "${afterBaseBean.min}/${afterBaseBean.max}℃"
-            )
-            if (views.layoutId == R.layout.today_weather_widget_large) {
-                // 构建适配器
-                val intent = Intent(context, TodayWeatherWidgetService::class.java).apply {
-                    // Add the app widget ID to the intent extras.
-                    putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
-                    putExtra(LOCATION_INFO, getLocation(cityInfo = cityInfo))
-                    data = Uri.parse(toUri(Intent.URI_INTENT_SCHEME))
-                }
-                // 设置 RemoteViews 对象以使用 RemoteViews 适配器
-                views.setRemoteAdapter(R.id.today_list_view, intent)
-            }
-        }
-        appWidgetManager.updateAppWidget(appWidgetId, views)
-        XLog.e("notifyWeatherWidget: ${items.size}")
     }
 }
