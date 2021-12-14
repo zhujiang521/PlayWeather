c7bb2390574c9c5432818ca86e8d113914598195
zhujiang2 <zhujiang2@lenovo.com>
2021-12-14 14:45:51 +0800

    优化代码

--- app/src/main/java/com/zj/weather/utils/BitmapFillet.kt
+++ app/src/main/java/com/zj/weather/utils/BitmapFillet.kt
@@ -93,7 +93,7 @@ object BitmapFillet {
      * @return 设置圆角后的图片
      */
     fun fillet(context: Context, bitmap: Bitmap, roundDp: Int, corners: Int = CORNER_ALL): Bitmap {
-        val roundPx = dp2px(context = context, dpValue = roundDp.toFloat())
+        val roundPx = context.dp2px(roundDp.toFloat())
         XLog.e("roundDp:$roundDp  roundPx:$roundPx   corners:$corners")
         return try {
             // 其原理就是：先建立一个与图片大小相同的透明的Bitmap画板
@@ -169,20 +169,20 @@ object BitmapFillet {
         canvas.drawRect(block, paint)
     }
 
-    /**
-     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
-     */
-    private fun px2dp(context: Context, pxValue: Float): Int {
-        val scale = context.resources.displayMetrics.density
-        return (pxValue / scale + 0.5f).toInt()
-    }
+}
 
-    /**
-     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
-     */
-    private fun dp2px(context: Context, dpValue: Float): Int {
-        val scale = context.resources.displayMetrics.density
-        return (dpValue * scale + 0.5f).toInt()
-    }
+/**
+ * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+ */
+private fun Context.px2dp(pxValue: Float): Int {
+    val scale = resources.displayMetrics.density
+    return (pxValue / scale + 0.5f).toInt()
+}
 
+/**
+ * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+ */
+private fun Context.dp2px(dpValue: Float): Int {
+    val scale = resources.displayMetrics.density
+    return (dpValue * scale + 0.5f).toInt()
 }
