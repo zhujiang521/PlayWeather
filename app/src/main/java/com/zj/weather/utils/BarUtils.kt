397cb4838feaa39d4aa2381c81849c63b5dd3611
zhujiang2 <zhujiang2@lenovo.com>
2021-12-15 11:19:55 +0800

    优化设置透明状态栏方法

--- app/src/main/java/com/zj/weather/utils/BarUtils.kt
+++ app/src/main/java/com/zj/weather/utils/BarUtils.kt
@@ -2,16 +2,15 @@ package com.zj.weather.utils
 
 import android.app.Activity
 import android.graphics.Color
-import android.view.View
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat.Type.statusBars
 
 /**
  * 设置透明状态栏
  */
 fun Activity.transparentStatusBar() {
-    val option = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-    val vis = window.decorView.systemUiVisibility
-    window.decorView.systemUiVisibility = option or vis
+    val controller = ViewCompat.getWindowInsetsController(window.decorView)
+    controller?.hide(statusBars())
     window.statusBarColor = Color.TRANSPARENT
 }
 
