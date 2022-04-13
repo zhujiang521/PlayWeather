a368e405feb4df281293a406378e7fb43c56c92b
zhujiang2 <zhujiang2@lenovo.com>
2022-04-13 22:09:06 +0800

    升级依赖版本并修改废弃方法

--- app/src/main/java/com/zj/weather/utils/BarUtils.kt
+++ app/src/main/java/com/zj/weather/utils/BarUtils.kt
@@ -1,31 +1,22 @@
 package com.zj.weather.utils
 
 import android.app.Activity
-import android.graphics.Color
-import android.view.View
-import android.view.WindowManager
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat.Type.ime
-import androidx.core.view.WindowInsetsCompat.Type.statusBars
 
 /**
  * 设置透明状态栏
  */
 fun Activity.transparentStatusBar() {
-    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-    val option = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-    val vis = window.decorView.systemUiVisibility
-    window.decorView.systemUiVisibility = option or vis
-    window.statusBarColor = Color.TRANSPARENT
+    WindowCompat.setDecorFitsSystemWindows(window, false)
 }
 
 /**
  * 状态栏反色
  */
 fun Activity.setAndroidNativeLightStatusBar() {
-    val controller = ViewCompat.getWindowInsetsController(window.decorView)
-    controller?.isAppearanceLightStatusBars = !isDarkMode()
+    val controller = WindowCompat.getInsetsController(window, window.decorView)
+    controller.isAppearanceLightStatusBars = !isDarkMode()
 }
 
 /**
@@ -33,8 +24,8 @@ fun Activity.setAndroidNativeLightStatusBar() {
  */
 fun Activity?.hideIme() {
     if (this == null || window == null) return
-    val controller = ViewCompat.getWindowInsetsController(window.decorView)
-    controller?.hide(ime())
+    val controller = WindowCompat.getInsetsController(window, window.decorView)
+    controller.hide(ime())
 }
 
 /**
@@ -42,6 +33,6 @@ fun Activity?.hideIme() {
  */
 fun Activity?.showIme() {
     if (this == null || window == null) return
-    val controller = ViewCompat.getWindowInsetsController(window.decorView)
-    controller?.show(ime())
+    val controller = WindowCompat.getInsetsController(window, window.decorView)
+    controller.show(ime())
 }
