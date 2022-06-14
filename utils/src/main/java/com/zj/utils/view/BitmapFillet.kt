fcf2d9cd86f9f92c92957ff2d790723337d934f3
zhujiang2 <zhujiang2@lenovo.com>
2022-06-14 10:42:37 +0800

    Update lib version

--- utils/src/main/java/com/zj/utils/view/BitmapFillet.kt
+++ utils/src/main/java/com/zj/utils/view/BitmapFillet.kt
@@ -173,14 +173,6 @@ object BitmapFillet {
 }
 
 /**
- * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
- */
-private fun Context.px2dp(pxValue: Float): Int {
-    val scale = resources.displayMetrics.density
-    return (pxValue / scale + 0.5f).toInt()
-}
-
-/**
  * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
  */
 private fun Context.dp2px(dpValue: Float): Int {
