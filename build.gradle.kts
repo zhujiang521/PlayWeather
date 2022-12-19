6409e9810641d302167e4f95d214edebf75f2aad
zhujiang2 <zhujiang2@lenovo.com>
2022-12-19 21:10:03 +0800

    升级 Compose 版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
-    id("org.jetbrains.compose") version "1.2.1"
+    id("org.jetbrains.compose") version "1.2.2"
 }
 
 group = "me.zhujiang"
