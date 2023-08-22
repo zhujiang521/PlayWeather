c4394d67e5c8f6be485ad37b30e5e15b0f22354f
zhujiang2 <zhujiang2@lenovo.com>
2023-08-22 16:48:25 +0800

    update lib version.

--- model/src/main/java/com/zj/model/room/dao/CityInfoDao.kt
+++ model/src/main/java/com/zj/model/room/dao/CityInfoDao.kt
@@ -20,32 +20,32 @@ interface CityInfoDao {
     fun getCityInfoList(): LiveData<List<CityInfo>>
 
     @Query("SELECT * FROM city_info where is_location = :isLocation")
-    suspend fun getIsLocationList(isLocation: Int = 1): List<CityInfo>
+    fun getIsLocationList(isLocation: Int = 1): List<CityInfo>
 
     @Query("SELECT COUNT(*) FROM city_info where name = :name")
-    suspend fun getHasLocation(name: String): Int
+    fun getHasLocation(name: String): Int
 
     @Query("SELECT * FROM city_info where is_index = 1")
-    suspend fun getIndexCity(): List<CityInfo>
+    fun getIndexCity(): List<CityInfo>
 
     @Query("SELECT COUNT(*) FROM city_info")
-    suspend fun getCount(): Int
+    fun getCount(): Int
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertList(cityInfoList: List<CityInfo>)
+    fun insertList(cityInfoList: List<CityInfo>)
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(CityInfo: CityInfo)
+    fun insert(cityInfo: CityInfo)
 
     @Update
-    suspend fun update(CityInfo: CityInfo): Int
+    fun update(cityInfo: CityInfo): Int
 
     @Delete
-    suspend fun delete(CityInfo: CityInfo): Int
+    fun delete(cityInfo: CityInfo): Int
 
     @Delete
-    suspend fun deleteList(cityInfoList: List<CityInfo>): Int
+    fun deleteList(cityInfoList: List<CityInfo>): Int
 
     @Query("DELETE FROM city_info")
-    suspend fun deleteAll()
+    fun deleteAll()
 }
